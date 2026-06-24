00dba531a97214fbfd658f17eb304e963ca0d310
Tamara Aguirre <tamara.henriquez1995@gmail.com>
2026-06-23 22:04:08 -0400

    schedule

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\TicketCategorizationCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-//Artisan::command(TicketCategorizationCommand::class)->everyMinute()->withoutOverlapping();
+Artisan::command(TicketCategorizationCommand::class)->everyMinute()->withoutOverlapping();
+
